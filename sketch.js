694d9a3f9938c842d31adfa73e9d9b0d9ffed880
Reclan1234 <75433930+Reclan1234@users.noreply.github.com>
2021-01-09 21:17:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+var pig1
+var log1
+var bird1
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,300,50,50);
+    box2 = new Box(300,250,50,50);
+    ground = new Ground(200,height,400,20);
+    pig1=new pig(250,200);
+    log1=new log(200,50,70,PI)
+    bird1=new bird(200,50,100,150)
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+    bird1.display();
+}
